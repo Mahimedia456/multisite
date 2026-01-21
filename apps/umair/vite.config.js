aeb3700b685e269c5d5f85c9441c91671cf183d2
Aamir Soomro <mahimedia@Umayrs-Mac-mini.local>
2026-01-21 21:52:07 +0500

    checknowadminheader8

--- apps/umair/vite.config.js
+++ apps/umair/vite.config.js
@@ -1,7 +1,20 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  server: {
+    proxy: {
+      "/public": {
+        target: "http://localhost:5050",
+        changeOrigin: true,
+        secure: false,
+      },
+      "/admin": {
+        target: "http://localhost:5050",
+        changeOrigin: true,
+        secure: false,
+      },
+    },
+  },
+});
